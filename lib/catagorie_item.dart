99dc91cd7bf71c8e53c4e2665d9060e7c392d7ec
MichaelGetuWorku <michael.getu.hh3221@gmail.com>
2021-09-12 21:11:21 +0300

    aded  catagorie items

---
+++ lib/catagorie_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CatagoryItem extends StatelessWidget {
+  late final String title;
+  late final Color color;
+
+  CatagoryItem(this.title, this.color);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
--- lib/catagories_screen.dart
+++ lib/catagories_screen.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:foddy/catagorie_item.dart';
+import 'dammy_data.dart';
 
 class CatagoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Foody'),
       ),
-      children: [
-        
-      ],
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CatagoryItem(
+                  catData.title,
+                  catData.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }
